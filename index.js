2df6d8bd4d030258052447656e518d87f163b970
Donald Sonn <dsonn@Donalds-MBP.fios-router.home>
2019-02-20 16:09:55 -0500

    added filter function

--- index.js
+++ index.js
@@ -57,5 +57,38 @@ function goodbye() {
     console.log('Goodbye world');
 }
 
-repeat(hello,5);
-repeat(goodbye,5);
+// repeat(hello,5);
+// repeat(goodbye,5);
+
+function filter(arr, fn) { // arr=myNames, fn=checker
+    newArray = []; // ['Rich', 'Ray']
+    for(i=0; i<arr.length;i++) {
+        if (fn(arr[i]) === true) { // checker(myNames[3]), checker('Ray'), true because Ray[0] !== 'R', true=true
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+}
+
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+    // This is known as a "predicate function" - it's a function that 
+    // only returns a boolean
+    return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
+
+// named function variant of line 79
+
+function checker(name) {
+    return name[0] === 'R'; // return the first letter of name and does it equal R?
+}
+
+const filteredNames2 = filter(myNames, checker);
